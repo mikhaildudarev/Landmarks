fd0fc1dd40fe6cc042cafc3f6ed3dfb1a6db054d
Mikhail Dudarev <github@dev.mikhaildudarev.com>
2021-07-22 13:16:08 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mikhail Dudarev on 22.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
