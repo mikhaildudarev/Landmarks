a5fb50a2ab4d68f473eaab19fb67fe1d6945bde2
Mikhail Dudarev <github@dev.mikhaildudarev.com>
2021-07-31 15:16:20 +0300

    Composing Complex Interfaces - Section 1

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Mikhail Dudarev on 31.07.2021.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
